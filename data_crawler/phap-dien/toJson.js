6313cbaf062c60ecfe888df09e00d3118baf425e
ducvan1909 <ducvan122006@gmail.com>
2026-05-19 14:40:32 +0700

    law structure

---
+++ data_crawler/phap-dien/toJson.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const vm = require('vm');
+const path = require('path');
+
+const inputFile = path.join(__dirname, 'jsonData.js');
+const outputDir = __dirname;
+
+const source = fs.readFileSync(inputFile, 'utf8');
+
+const sandbox = { window: {}, global: {} };
+vm.createContext(sandbox);
+vm.runInContext(source, sandbox);
+
+const chude = sandbox.jdChuDe || sandbox.window.jdChuDe || sandbox.global.jdChuDe;
+const demuc = sandbox.jdDeMuc || sandbox.window.jdDeMuc || sandbox.global.jdDeMuc;
+const treeNode = sandbox.jdAllTree || sandbox.window.jdAllTree || sandbox.global.jdAllTree;
+
+if (!chude || !demuc || !treeNode) {
+  throw new Error('Khong doc duoc jdChuDe, jdDeMuc, hoac jdAllTree tu jsonData.js');
+}
+
+fs.writeFileSync(path.join(outputDir, 'chude.json'), JSON.stringify(chude, null, 2), 'utf8');
+fs.writeFileSync(path.join(outputDir, 'demuc.json'), JSON.stringify(demuc, null, 2), 'utf8');
+fs.writeFileSync(path.join(outputDir, 'treeNode.json'), JSON.stringify(treeNode, null, 2), 'utf8');
+
+console.log('Da tao chude.json, demuc.json, treeNode.json');
