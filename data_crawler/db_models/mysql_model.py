99fcbfeb103321f8a8f0526673d0437cf0fe9157
ducvan1909 <ducvan122006@gmail.com>
2026-05-19 21:12:41 +0700

    use peewee to create tables in mysql

---
+++ data_crawler/db_models/mysql_model.py
@@ -0,0 +1,50 @@
+from peewee import *
+from db_config import db
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class PDChuDe(BaseModel):
+    chude_id = CharField(max_length=64, primary_key=True)
+    ten = TextField()
+    stt = IntegerField()
+
+class PDDeMuc(BaseModel):
+    demuc_id = CharField(max_length=64, primary_key=True)
+    ten = TextField()
+    stt = IntegerField()
+    chude_id = ForeignKeyField(PDChuDe, backref='demucs')
+
+class PDChuong(BaseModel):
+    chuong_id = CharField(max_length=64, primary_key=True)
+    chimuc = TextField()
+    ten = TextField()
+    stt = IntegerField()
+    demuc_id = ForeignKeyField(PDDeMuc, backref='chuongs')
+    chude_id = ForeignKeyField(PDChuDe, backref='chuongs')
+
+class PDDieu(BaseModel):
+    dieu_id = CharField(max_length=64, primary_key=True)
+    ten = TextField()
+    stt = IntegerField()
+    noi_dung = TextField()
+    chi_muc = IntegerField()
+    ten_vbqppl = TextField()
+    link_vbqppl = TextField(null=True)
+    chuong_id = ForeignKeyField(PDChuong, backref='dieus')
+    demuc_id = ForeignKeyField(PDDeMuc, backref='dieus')
+    chude_id = ForeignKeyField(PDChuDe, backref='dieus')
+
+class PDBang(BaseModel):
+    dieu_id = ForeignKeyField(PDDieu, backref='bangs')
+    html = TextField()
+
+class PDFile(BaseModel):
+    dieu_id = ForeignKeyField(PDDieu, backref='files')
+    path = TextField()
+    link = TextField()
+
+class PDDieuLienQUan(BaseModel):
+    dieu1 = ForeignKeyField(PDDieu)
+    dieu2 = ForeignKeyField(PDDieu)
