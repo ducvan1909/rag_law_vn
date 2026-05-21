c28170c32560680f52e73a7e0c15a3fbbf1f1158
Duc Van <ducvan122006@gmail.com>
2026-05-21 20:52:27 +0700

    change max char length of id

--- data_crawler/db_models/mysql_model.py
+++ data_crawler/db_models/mysql_model.py
@@ -1,30 +1,32 @@
 from peewee import *
 from db_config import db
 
+MAX_LENGTH = 128
+
 class BaseModel(Model):
     class Meta:
         database = db
 
 class PDChuDe(BaseModel):
-    chude_id = CharField(max_length=64, primary_key=True)
+    chude_id = CharField(max_length=MAX_LENGTH, primary_key=True)
     ten = TextField()
     stt = IntegerField()
 
 class PDDeMuc(BaseModel):
-    demuc_id = CharField(max_length=64, primary_key=True)
+    demuc_id = CharField(max_length=MAX_LENGTH, primary_key=True)
     ten = TextField()
     stt = IntegerField()
     chude_id = ForeignKeyField(PDChuDe, backref='demucs')
 
 class PDChuong(BaseModel):
-    chuong_id = CharField(max_length=64, primary_key=True)
+    chuong_id = CharField(max_length=MAX_LENGTH, primary_key=True)
     chimuc = TextField()
     ten = TextField()
     stt = IntegerField()
     demuc_id = ForeignKeyField(PDDeMuc, backref='chuongs')
 
 class PDDieu(BaseModel):
-    dieu_id = CharField(max_length=64, primary_key=True)
+    dieu_id = CharField(max_length=MAX_LENGTH, primary_key=True)
     ten = TextField()
     stt = IntegerField()
     noi_dung = TextField()
