7decab8c0069871452a208de3ffc901cfcfd5465
ducvan1909 <ducvan122006@gmail.com>
2026-05-22 17:17:10 +0700

    feat: crawl pháp điển thành công

--- data_crawler/main.py
+++ data_crawler/main.py
@@ -3,6 +3,7 @@ import pathlib
 import os
 import uuid
 
+from collections import defaultdict
 from bs4 import BeautifulSoup
 from db_config import db
 from db_models.mysql_model import *
@@ -44,8 +45,16 @@ with open("./phap-dien/treeNode.json", "r", encoding="utf_8") as f_tree_nodes:
     tree_nodes = json.load(f_tree_nodes)
 f_tree_nodes.close()
 
+#Biến các node thành dictionary với key là DeMucID, value là list các node tương ứng
+tree_nodes_dict = defaultdict(list)
+for node in tree_nodes:
+    tree_nodes_dict[node["DeMucID"]].append(node)
+
 demuc_dir = pathlib.Path("./phap-dien/demuc")
 dieus_lienquan = []
+
+inserted_chuong = set()
+inserted_dieu = set()
 
 for file in os.listdir(demuc_dir):
     filename = os.fsdecode(file)
@@ -54,28 +63,33 @@ for file in os.listdir(demuc_dir):
         with open(filepath, "r", encoding="utf_8") as demuc_file:
             demuc_html = demuc_file.read()
             demuc_html = BeautifulSoup(demuc_html, "html.parser")
-            demuc_node = [node for node in tree_nodes if node["DeMucID"] == filename.split(".")[0]]
+            demuc_node = tree_nodes_dict.get(filename.split(".")[0], [])
+            demuc_chuong = [node for node in demuc_node if node["TEN"].startswith("Chương ")]
+            demuc_dieu = [node for node in demuc_node if node["TEN"].startswith("Điều ")]
+
             if len(demuc_node) == 0:
                 print("Không tồn tại node cho đề mục:" + filename)
                 demuc_file.close()
                 continue
-            demuc_chuong = [node for node in demuc_node if node["TEN"].startswith("Chương ")]
 
             #Insert Chương vào db
             chuongs_data = []
             for chuong in demuc_chuong:
+                if chuong["MAPC"] in inserted_chuong:
+                    continue
                 try:
-                    chuong_data = PDChuong.create(
+                    chuongs_data.append(PDChuong(
                         chuong_id = chuong["MAPC"],
                         chimuc = chuong["ChiMuc"],
                         ten = chuong["TEN"],
                         stt = roman_to_int(chuong["ChiMuc"]),
                         demuc_id = filename.split(".")[0]
-                    )
+                    ))
+                    inserted_chuong.add(chuong["MAPC"])
                 except:
                     continue
-                chuongs_data.append(chuong_data)
             print(f'Inserted {len(chuongs_data)} chương của đề mục {filename.split(".")[0]}')
+            PDChuong.bulk_create(chuongs_data)
 
             #Tạo chương giả nếu không tồn tại chương
             if len(chuongs_data) == 0:
@@ -88,12 +102,15 @@ for file in os.listdir(demuc_dir):
                 )
                 chuongs_data.append(chuong_data)
 
-            demuc_dieu = [node for node in demuc_node if node["TEN"].startswith("Điều ")]
-
             print(f'Đề mục {filename.split(".")[0]} có {len(demuc_chuong)} chương và {len(demuc_dieu)} điều')
 
             stt = 0
+            dieus_data = []
+            tables = []
+            files = []
             for dieu in demuc_dieu:
+                if dieu["MAPC"] in inserted_dieu:
+                    continue
                 if len(chuongs_data) == 1:
                     chuong_id = chuongs_data[0].chuong_id
                 else:
@@ -114,14 +131,14 @@ for file in os.listdir(demuc_dir):
                 vbqppl_link = ghi_chu_html.select("a")[0]["href"] if ghi_chu_html and ghi_chu_html.select("a") else None
                 noidung_html = dieu_html.parent.find_next("p", {"class": "pNoiDung"})
                 noidung = ""
-                tables = []
+
                 for content in noidung_html.contents:
                     if content.name == "table":
-                        tables.append(str(content))
+                        tables.append(PDBang(dieu_id=dieu["MAPC"], html=str(content)))
                         continue
                     noidung += str(content.text.strip()) + "\n"
                 try:
-                    PDDieu.create(
+                    dieus_data.append(PDDieu(
                         dieu_id = dieu["MAPC"],
                         ten = dieu["TEN"],
                         stt = stt,
@@ -132,26 +149,22 @@ for file in os.listdir(demuc_dir):
                         chuong_id = chuong_id,
                         demuc_id = dieu["DeMucID"],
                         chude_id = demuc_db.chude_id_id
-                    )
+                    ))
+                    inserted_dieu.add(dieu["MAPC"])
                 except Exception as e:
                     print(f'Lỗi insert điều {dieu["MAPC"]}: {e}')
                     continue
-
-                for table in tables:
-                    PDBang.create(dieu_id=dieu["MAPC"], html=table)
-
                 element = noidung_html.next_sibling
                 # Lấy link các file, biếu mẫu nếu có đính kèm
                 while element and element.name == "a":
 
                     link = element["href"]
                     try:
-                        PDFile.create(dieu_id=dieu["MAPC"], link=link, path="")
-                    except:
-                        print("Lỗi insert file" + link)
+                        files.append(PDFile(dieu_id=dieu["MAPC"], link=link, path=""))
+                    except Exception as e:
+                        print(f"Lỗi insert file {link}: {e}")
 
                     element = element.next_sibling
-
                 # Lấy các điều có liên quan:
 
                 if element and element.name == "p" and element["class"] and element["class"][0] == "pChiDan":
@@ -163,25 +176,23 @@ for file in os.listdir(demuc_dir):
                         dieus_lienquan.append({"dieu_id1": dieu["MAPC"], "dieu_id2": dieu_id_lienquan})
 
                 stt += 1
+            PDDieu.bulk_create(dieus_data)
+            PDBang.bulk_create(tables)
+            PDFile.bulk_create(files)
             demuc_file.close()
 
-existing_dieu_ids = {row.dieu_id for row in PDDieu.select(PDDieu.dieu_id)}
+dieus_lienquan_data = []
 
 for dieu_lienquan in dieus_lienquan:
-    if dieu_lienquan["dieu_id2"] not in existing_dieu_ids:
-        print(
-            f'missing_target điều liên quan {dieu_lienquan["dieu_id1"]} - {dieu_lienquan["dieu_id2"]}'
-        )
-        continue
     try:
-        PDDieuLienQUan.create(
+        dieus_lienquan_data.append(PDDieuLienQUan(
             dieu1 = dieu_lienquan["dieu_id1"],
             dieu2 = dieu_lienquan["dieu_id2"]
-        )
+        ))
     except Exception as e:
         print(f'Không thể insert điều liên quan {dieu_lienquan["dieu_id1"]} - {dieu_lienquan["dieu_id2"]}: {e}')
 
-
+PDDieuLienQUan.bulk_create(dieus_lienquan_data)
 
 
 
