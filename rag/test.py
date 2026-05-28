90c7b7a5ca768e8b1cef044194cacbd2d22b9553
ducvan1909 <ducvan122006@gmail.com>
2026-05-28 17:35:17 +0700

    creating vectorizer for rag

---
+++ rag/test.py
@@ -0,0 +1,57 @@
+import os
+from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer
+
+from database.db_config import db
+from database.mysql_model import VBPLUnit
+
+MAX_TOKENS = 700
+
+load_dotenv()
+
+model_name = os.getenv("EMBEDDING_MODEL")
+hf_token = os.getenv("HF_TOKEN")
+
+embedding_model = SentenceTransformer(model_name, token=hf_token)
+
+def get_vbpl_unit(limit=100):
+    db.connect(reuse_if_open=True)
+
+    query = (
+        VBPLUnit
+        .select()
+        .where(VBPLUnit.content.is_null(False) and VBPLUnit.content != "")
+        .limit(limit)
+        .dicts()
+    )
+
+    rows = list(query)
+
+    db.close()
+    return rows
+
+def get_content(limit=100):
+    db.connect(reuse_if_open=True)
+
+    query = (
+        VBPLUnit
+        .select(VBPLUnit.content)
+        .where(VBPLUnit.content.is_null(False) and VBPLUnit.content != "")
+        .limit(limit)
+        .dicts()
+    )
+
+    db.close()
+    rows = list(query)
+    print(rows)
+
+    return rows
+
+get_content(2)
+
+def get_tokens_length(tokenizer, corpus):
+    tokens = tokenizer.encode(corpus, add_special_tokens=False)
+    return len(tokens)
+
+
+
