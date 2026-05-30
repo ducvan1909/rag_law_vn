65b84cf646b3fc288482c22cad2307d46ddb6693
Duc Van <ducvan122006@gmail.com>
2026-05-30 08:26:29 +0700

    add .gitattributes

--- rag/test.py
+++ rag/test.py
@@ -1,4 +1,6 @@
 import os
+from asyncio.windows_events import NULL
+
 from sentence_transformers import SentenceTransformer
 
 from database.db_config import db
@@ -27,24 +29,32 @@ def get_vbpl_unit(limit=100):
     db.close()
     return rows
 
-def get_content(limit=100):
+def get_content(limit=None):
     db.connect(reuse_if_open=True)
 
-    query = (
-        VBPLUnit
-        .select(VBPLUnit.content)
-        .where(VBPLUnit.content.is_null(False) and VBPLUnit.content != "")
-        .limit(limit)
-        .dicts()
-    )
-
+    if limit is None:
+        query = (
+            VBPLUnit
+            .select(VBPLUnit.content)
+            .where(VBPLUnit.content.is_null(False) and VBPLUnit.content != "")
+            .dicts()
+        )
+    else:
+        query = (
+            VBPLUnit
+            .select(VBPLUnit.content)
+            .where(VBPLUnit.content.is_null(False) and VBPLUnit.content != "")
+            .limit(limit)
+            .dicts()
+        )
     db.close()
     rows = list(query)
-    print(rows)
+    data = [row["content"] for row in rows]
+    return data
 
-    return rows
-
-get_content(2)
+contents = get_content()
+for content in contents:
+    print(content)
 
 def get_tokens_length(tokenizer, corpus):
     tokens = tokenizer.encode(corpus, add_special_tokens=False)
